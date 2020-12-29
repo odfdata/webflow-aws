9fc19f491d2e827aef3ff60697a745f4027a111a
Federico Castelli <f.castelli@ianum.com>
2020-12-29 11:42:08 +0100

    Created setup.py and requirements.txt file to start the development

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='webflow-aws',
+    version='0.0.1',
+    author='CreateInCloud',
+    author_email='help@createin.cloud',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=requirements,
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent'
+    ]
+)
