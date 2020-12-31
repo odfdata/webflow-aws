639bdef78b9ce48db3c2375573c3f9e96c3bc3b5
Federico Castelli <f.castelli@ianum.com>
2020-12-31 15:13:46 +0100

    Created configuration.json file to set parameters

--- scripts/script_cloudformation_update.py
+++ scripts/script_cloudformation_update.py
@@ -1,19 +1,27 @@
+import json
+
 import boto3
 
 if __name__ == '__main__':
+    with open('../configuration.json') as f:
+        configuration = json.load(f)
     with open('../template_webflow_aws.yaml') as f:
         template_body = f.read()
     client = boto3.client('cloudformation')
 
     response = client.update_stack(
-        StackName='CreateInCloudTest',
+        StackName=configuration['stack_name'],
         TemplateBody=template_body,
         UsePreviousTemplate=False,
         Capabilities=['CAPABILITY_IAM'],
         Parameters=[
             {
                 'ParameterKey': 'BucketName',
-                'ParameterValue': 'test.createin.cloud'
+                'ParameterValue': configuration['bucket_name']
+            },
+            {
+                'ParameterKey': 'SupportBucketName',
+                'ParameterValue': configuration['support_bucket_name']
             }
         ]
 
--- scripts/script_setup.py
+++ scripts/script_setup.py
@@ -1,3 +1,4 @@
+import json
 from time import sleep
 
 import boto3
@@ -5,15 +6,23 @@ import boto3
 if __name__ == '__main__':
     cloudformation_client = boto3.client('cloudformation')
     s3_resource = boto3.resource('s3')
+    with open('../configuration.json') as f:
+        configuration = json.load(f)
     with open('../template_setup.yaml') as f:
         template_setup = f.read()
 
     response = cloudformation_client.create_stack(
-        StackName='WebflowAWSSupport',
+        StackName=configuration['support_stack_name'],
         TemplateBody=template_setup,
         TimeoutInMinutes=5,
         Capabilities=['CAPABILITY_IAM'],
-        OnFailure='DO_NOTHING'
+        OnFailure='DO_NOTHING',
+        Parameters=[
+            {
+                'ParameterKey': 'BucketName',
+                'ParameterValue': configuration['support_bucket_name']
+            }
+        ]
     )
     stack_id = response['StackId']
     
@@ -25,7 +34,7 @@ if __name__ == '__main__':
             break
     print('Stack successfully created')
     s3_resource.meta.client.upload_file(
-        Bucket='webflow-aws-support',
+        Bucket=configuration['support_bucket_name'],
         Filename='../lambda_function/s3_trigger_artifacts_upload/s3_trigger_upload_artifacts.zip',
         Key='lambda_function/s3_trigger_artifacts_upload/package.zip'
     )
