0bb3b2b89a7c65558e33a935b5d0bb27f8c295a5
Federico Castelli <f.castelli@ianum.com>
2021-01-20 16:42:06 +0100

    Added setup.py properties
    + Added description, long_description. url and license properties
    + Added the development status and the natural language in classifiers

--- setup.py
+++ setup.py
@@ -6,13 +6,19 @@ with open('requirements.txt') as f:
 setup(
     name='webflow-aws',
     version='0.0.0',
+    description='Deploy your Webflow static website on AWS',
+    long_description=open('README.md').read(),
     author='CreateInCloud',
     author_email='help@createin.cloud',
+    url='https://github.com/CreateInCloud/webflow-aws',
     packages=find_packages(),
     python_requires='>=3.6',
     install_requires=requirements,
     include_package_data=True,
+    license="Apache License 2.0",
     classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3.6',
         'Operating System :: OS Independent'
