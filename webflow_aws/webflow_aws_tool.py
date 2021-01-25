bb8d237f6db0465858f0d1e10c6767f8c324d038
Federico Castelli <f.castelli@ianum.com>
2021-01-25 15:41:06 +0100

    Removed aws_region_name required parameter

--- webflow_aws/webflow_aws_tool.py
+++ webflow_aws/webflow_aws_tool.py
@@ -3,13 +3,14 @@ import json
 import os
 import shutil
 from time import sleep
-from aws_cdk import core
+
 import boto3
 import click
 
-from webflow_aws.create_cloudformation_template import WebflowAWSStack
-from webflow_aws.utils import configuration_yaml_exists, websites_folder_exists, get_configuration, \
-    get_setup_bucket_name, setup_bucket_exists
+from webflow_aws.utils import configuration_yaml_exists, get_configuration, get_setup_bucket_name
+
+
+aws_region_name = 'us-east-1'
 
 
 @click.group()
@@ -21,7 +22,7 @@ def cli():
 
 @cli.command(short_help='Create the configuration.yaml file')
 def create_config():
-    click.echo('Future release')
+    click.echo('Feature release')
 
 
 @cli.command(short_help="Publish your website in production")
@@ -29,21 +30,16 @@ def publish():
     # check if the configuration.yaml file exists
     if not configuration_yaml_exists():
         click.echo(
-            'The configuration.yaml file doesn\'t exist. Read the README.md file to see how to create it', err=True)
-        return
-    # check if the websites folder exists
-    if not websites_folder_exists():
-        click.echo('The websites folder doesn\'t exist')
+            'The webflow-aws-config.yaml file doesn\'t exist. Read the README.md file to see how to create it',
+            err=True)
         return
     # check if there's a .zip file inside the websites folder
-    zip_files = glob.glob('./websites/*.zip')
+    zip_files = glob.glob('./*.zip')
     if not zip_files:
-        click.echo('The websites folder doesn\'t contain a .zip file')
+        click.echo('The folder doesn\'t contain a .zip file')
         return
     configuration = get_configuration()
-    session = boto3.session.Session(
-        profile_name=configuration.get('aws_profile_name', 'default'),
-        region_name=configuration['aws_region_name'])
+    session = boto3.session.Session(profile_name=configuration.get('aws_profile_name', 'default'))
     # nano cdk.json
     with open('cdk.json', 'w') as outfile:
         json.dump({'app': 'python3 app.py'}, outfile)
@@ -61,17 +57,14 @@ def publish():
     click.echo('Upload completed')
 
 
-@cli.command(
-    short_help='Create all the needed resources to publish your website. If the configuration.yaml file doesn\'t exist,'
-               'it\'ll guide you through the creation process')
+@cli.command(short_help='Create all the needed resources to publish your website')
 def setup():
     if not configuration_yaml_exists():
         click.echo(
-            'The configuration.yaml file doesn\'t exist. Read the README.md file to see how to create it', err=True)
+            'The webflow-aws-config.yaml file doesn\'t exist. Read the README.md file to see how to create it',
+            err=True)
     configuration = get_configuration()
-    session = boto3.session.Session(
-        profile_name=configuration.get('aws_profile_name', 'default'),
-        region_name=configuration['aws_region_name'])
+    session = boto3.session.Session(profile_name=configuration.get('aws_profile_name', 'default'))
     cloudformation_client = session.client(service_name='cloudformation')
     click.echo('Going to create all the needed resources.')
     # check if the support stack is already created
@@ -80,8 +73,7 @@ def setup():
         stack_info for stack_info in response.get('Stacks', [])
         if stack_info.get('StackName', '') == configuration['support_stack_name']]
     setup_bucket_name = get_setup_bucket_name(
-        aws_profile_name=configuration.get('aws_profile_name', 'default'),
-        aws_region_name=configuration['aws_region_name'])
+        aws_region_name=aws_region_name, aws_profile_name=configuration.get('aws_profile_name', 'default'))
     if not already_created_stack:
         # create the support stack and wait for the creation complete
         with open(os.path.dirname(os.path.abspath(__file__)) + '/templates/template_setup.yaml') as f:
@@ -123,6 +115,4 @@ def setup():
             os.path.abspath(__file__)) + '/lambda_function/s3_trigger_artifacts_upload/s3_trigger_upload_artifacts.zip',
         Key='lambda_function/s3_trigger_artifacts_upload/package.zip'
     )
-    click.echo('Everything has been created. Now you need to run this command: cdk deploy. If you configured a '
-               'different aws_profile_name, you have to remember to specify the --profile {your_profile_name} '
-               'parameter')
+    click.echo('Everything has been created. Now you need to run this command: webflow-aws publish')
