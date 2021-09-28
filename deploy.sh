e31a320669128ba839592467cbfa7c05901b7b3b
Federico <f.castelli@ianum.com>
2021-09-28 16:01:12 +0200

    Fixed requirements for the generated python package

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-pip3 install twine
 rm -rf build
 rm -rf dist
 python3 setup.py sdist bdist_wheel
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-# with open('requirements.txt') as f:
-#     requirements = f.read().splitlines()
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup(
     name='webflow-aws',
@@ -10,28 +10,11 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     author='CreateInCloud',
-    author_email='it@createin.cloud',
+    author_email='fede.castelli21@gmail.com',
     url='https://github.com/CreateInCloud/webflow-aws',
     packages=find_packages(),
     python_requires='>=3.6',
-    install_requires=[
-        'boto3~=1.16.46',
-        'botocore~=1.19.46',
-        'pyyaml~=5.3.1',
-        'aws-cdk.core~=1.86.0',
-        'aws-cdk.aws-certificatemanager~=1.86.0',
-        'aws-cdk.aws-cloudfront~=1.86.0',
-        'aws-cdk.aws-s3~=1.86.0',
-        'aws-cdk.aws-s3-notifications~=1.86.0',
-        'aws-cdk.aws-lambda~=1.86.0',
-        'aws-cdk.aws-iam~=1.86.0',
-        'aws-cdk.aws-cloudfront-origins~=1.86.0',
-        'aws-cdk.aws-route53~=1.86.0',
-        'aws-cdk.aws-route53-targets~=1.86.0',
-        'Click~=7.1.2',
-        'tqdm~=4.56.0',
-        'emoji~=1.2.0'
-    ],
+    install_requires=requirements,
     include_package_data=True,
     license="Apache License 2.0",
     classifiers=[
